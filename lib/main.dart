0a236ebe7e67d6d8455c15f8fb2aec7411fc60ac
sa8a <jpkrllll@yahoo.co.jp>
2022-12-01 18:34:57 +0900

    add: Cloud Firestoreにデータを入力・反映できるように

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,6 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   final TextEditingController _controller = TextEditingController();
 
-  final _list = List.generate(10, (index) => 'test $index');
-
   @override
   void dispose() {
     _controller.dispose();
@@ -43,7 +41,6 @@ class _MyWidgetState extends State<MyWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final reverseList = _list.reversed.toList();
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -55,14 +52,45 @@ class _MyWidgetState extends State<MyWidget> {
                 child: Container(
                   height: double.infinity,
                   alignment: Alignment.topCenter,
-                  child: ListView.builder(
-                    itemCount: reverseList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Center(
-                        child: Text(
-                          reverseList[index],
-                          style: const TextStyle(fontSize: 20),
-                        ),
+
+                  // StreamBuilderにStreamの型としてQuerySnapshotを明示
+                  child: StreamBuilder<QuerySnapshot>(
+                    // streamの設定部分で、前述したデータの取得処理を記載
+                    // Cloud FirestoreからStreamでデータを取得
+                    // FirebaseFirestore.instanceでCloud Firestoreのデータベースのインスタンスを取得します。
+                    // collection(~)でコレクションを選択
+                    // orderBy(~)でフィールドの値にて並べ替え
+                    // 最後にsnapshots()でStreamにてデータの取得を行う
+                    stream: FirebaseFirestore.instance
+                        .collection('dream')
+                        .orderBy('createdAt')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('エラーが発生しました');
+                      }
+                      if (!snapshot.hasData) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      final list = snapshot.requireData.docs
+                          .map<String>((DocumentSnapshot document) {
+                        final documentData =
+                            document.data()! as Map<String, dynamic>;
+                        return documentData['content']! as String;
+                      }).toList();
+
+                      final reverseList = list.reversed.toList();
+
+                      return ListView.builder(
+                        itemCount: reverseList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Center(
+                            child: Text(
+                              reverseList[index],
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          );
+                        },
                       );
                     },
                   ),
@@ -78,10 +106,24 @@ class _MyWidgetState extends State<MyWidget> {
                   ),
                   ElevatedButton(
                     onPressed: () {
-                      setState(() {
-                        _list.add(_controller.text);
-                        _controller.clear();
-                      });
+                      // 保存するデータの定義（Map型のデータにて保存）
+                      // createdAtはDateTime型をTimeStamp型に変換するため、
+                      // このような処理を行っています。
+                      final document = <String, dynamic>{
+                        'content': _controller.text,
+                        'createdAt': Timestamp.fromDate(DateTime.now()),
+                      };
+
+                      // Cloud Firestoreへのデータ保存
+                      // collection : Firebaseで設定したコレクション名
+                      // doc : 空欄にするとdocument IDを自動で設定
+                      FirebaseFirestore.instance
+                          .collection('dream')
+                          .doc()
+                          .set(document);
+
+                      // 送信したらフォームをクリアする
+                      setState(_controller.clear);
                     },
                     child: const Text('送信'),
                   )
